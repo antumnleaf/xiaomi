0489b6deabf5bac335c6d5f362b99547137d55b1
antumnleaf <2411424598@qq.com>
2018-03-13 11:55:01 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+//nav效果
+{
+	let dropbox=document.querySelectorAll(".nav_drop");
+	let text=document.querySelectorAll(".nav_text span");
+	let obj=dropbox[0];
+	text.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			obj.style.display="none";
+			dropbox[index].style.display="block";
+			obj=dropbox[index];
+		}
+		ele.onmouseleave=function(){
+			dropbox[index].style.display="none";
+		}
+	});
+}
 // banner效果
 {
 	let imgs=document.querySelectorAll(".banner_img img");
@@ -64,6 +80,22 @@
 		});
 	});
 }
+//banner列表效果
+{
+	let labels=document.querySelectorAll(".banner_nav li");
+	let menus=document.querySelectorAll(".banner_nav_con");
+	let obj=menus[0];
+	labels.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			obj.style.display="none";
+			menus[index].style.display="block";
+			obj=menus[index];
+		}
+		ele.onmouseleave=function(){
+			menus[index].style.display="none";
+		}
+	});
+}
 //明星单品效果
 {
 	const prev=document.querySelector(".star_btn1");
@@ -100,7 +132,6 @@
 	function content(parent){
 		const types=parent.querySelectorAll(".dapei_right span");
 		const cons=parent.querySelectorAll(".dapei_bottom_right");
-		console.log(cons);
 		types.forEach(function(ele,index){
 			ele.onmouseenter=function(){
 				for(let i=0;i<types.length;i++){
@@ -113,8 +144,60 @@
 		})
 	}
 	const contentlist=document.querySelectorAll(".dapei");
-	console.log(contentlist);
 	content(contentlist[0]);
 	content(contentlist[1]);
 	content(contentlist[2]);
+}
+//内容效果
+{
+	function wheel(parent){
+		let prev=parent.querySelector(".content_prev");
+		let next=parent.querySelector(".content_next");
+		let inner=parent.querySelector(".content_item1");
+		let contents=parent.querySelectorAll(".content_con");
+		let pagers=parent.querySelectorAll(".content_dian_item");
+		console.log(inner);
+
+		let n=0;
+		let l=contents.length;
+		let obj=pagers[0];
+		next.onclick=function(){
+			n++;
+			if(n==l){
+				n=l-1;
+				return;
+			}
+			inner.style.marginLeft=-296*n+"px";
+			pagers[n].classList.add("content_active");
+			pagers[n-1].classList.remove("content_active");
+			obj=pagers[n];
+		}
+		prev.onclick=function(){
+			n--;
+			if(n<0){
+				n=0;
+				return;
+			}
+			inner.style.marginLeft=-296*n+"px";
+			pagers[n].classList.add("content_active");
+			pagers[n+1].classList.remove("content_active");
+			obj=pagers[n];
+		}
+		
+		pagers.forEach(function(ele,index){
+			ele.onclick=function(){
+				obj.classList.remove("content_active");
+				this.classList.add("content_active");
+				obj=this;
+				n=index;
+				inner.style.marginLeft=-296*n+"px";
+			}
+		});
+	}
+	let items=document.querySelectorAll(".content_item");
+	console.log(items);
+	items.forEach(function(ele,index){
+		wheel(ele);
+	});
+	
 }
